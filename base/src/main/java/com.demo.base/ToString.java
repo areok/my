cf00ea6a1dbe57b56c0039678b631c123cdb5326
马宇驰 <yuchi_ma@hzy-inv.com>
2017-11-09 14:22:06 +0800

    11_8

---
+++ base/src/main/java/com.demo.base/ToString.java
@@ -0,0 +1,14 @@
+package com.demo.base;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+/**
+ * Created by yangzhenwei on 2017/5/26.
+ */
+public abstract class ToString {
+
+    @Override
+    public String toString(){
+        return ToStringBuilder.reflectionToString(this);
+    }
+}
