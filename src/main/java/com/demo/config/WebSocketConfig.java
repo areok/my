b063ee86337b700c851e52c358fc60e227b3cd4c
马宇驰 <yuchi_ma@hzy-inv.com>
2017-11-14 09:12:53 +0800

    11_14

--- src/main/java/com/demo/config/WebSocketConfig.java
+++ src/main/java/com/demo/config/WebSocketConfig.java
@@ -20,24 +20,4 @@ public class WebSocketConfig {
     }
 
 }
-//@Configuration
-//@EnableWebSocketMessageBroker//1
-//public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
-//    @Override
-//    public void registerStompEndpoints(StompEndpointRegistry registry) {
-//        registry.addEndpoint("/endpointWisely").withSockJS(); //2
-//    }
-//
-//    @Override
-//    public void configureMessageBroker(MessageBrokerRegistry registry) {
-//        registry.enableSimpleBroker("/topic");//3
-//    }
-//}
 
-/**
- * (1),@EnableWebSocketMessageBroker注解用于开启使用STOMP协议来传输基于代理（MessageBroker）的消息，这时候控制器（controller）开始支持@MessageMapping,就像是使用@requestMapping一样。
- * <p>
- * (2),注册一个Stomp的节点（endpoint）,并指定使用SockJS协议。
- * <p>
- * (3),配置消息代理（MessageBroker）。
- */
